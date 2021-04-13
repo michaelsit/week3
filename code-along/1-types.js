17e4cb126226e675fd3bb3e583fdf75dcbe4c2d5
michaelsit <81634553+michaelsit@users.noreply.github.com>
2021-04-13 10:58:58 -0500

    week3

--- code-along/1-types.js
+++ code-along/1-types.js
@@ -2,9 +2,13 @@
 // Write the algorithm, in the comments. Write the code.
 
 // Store the item I want to eat
+let food = `tacos`
 
 // Store the quantity I want
+let quantity = 3
 
 // Make an English sentence
+let sentence = `I would like ${quantity} ${food}, please.`
 
 // Write the sentence to the console
+console.log(sentence)
--- lab/week3.js
+++ lab/week3.js
@@ -31,41 +31,55 @@ document.addEventListener('DOMContentLoaded', async function() {
 
   // PART 1
   // Figure out how products there are, and store that number in memory
-
+  let numProducts = data.products.length
   // Figure out how promotions there are, and store that number in memory
-
+  let numPromotions = data.promotions.length
   // Write the number of products to the JavaScript console
-
+  console.log(numProducts)
   // Write the number of promotions to the JavaScript console
+  console.log(numPromotions)
 
   // PART 2
   // Store the first product in memory
-
+  let product1 = data.products[1]
   // Write the first product to the JavaScript console, on three separate lines
-
+  console.log(product1)
   // Repeat for the 2nd and 3rd products
   // Store the second product in memory
-
+  let product2 = data.products[2]
   // Write the product to the JavaScript console, on three separate lines
-  
+  console.log(product2)
   // Store the third product in memory
-
+  let product3 = data.products[3]
   // Write the product to the JavaScript console, on three separate lines
-
+  console.log(product3)
+  
   // PART 3
   // Get a random integer between 1 and 10
-
   // Subtract one from that random integer because Arrays are zero-based
-
+  let randNum = getRandomInt(10) - 1
+  
   // Use the random integer to grab a promotion from the Array of promotions
   // and store it in memory
+  let promo = data.promotions[randNum]
 
   // Figure out which emoji should be displayed, store it in memory
 
+  let emojiNew
+  if (promo.type == `discount`) {
+    emojiNew = `🤑`
+  } else if (promo.type ==`shipping`) {
+    emojiNew = `🚛`
+  } else {
+    emojiNew = `🏆`
+  }
+
   // Create a sentence that displays the emoji along with the description 
   // of the promotion
+  let sentence = `${promo.type} - ${emojiNew}: ${promo.description}`
 
   // Write the sentence to the JavaScript console
+  console.log(sentence)
 
   // 🔥 YOUR CODE ENDS HERE 🔥
 })
